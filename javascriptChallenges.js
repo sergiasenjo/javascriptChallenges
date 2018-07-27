27ed4bf7d85bc0a653ba5ee8fff9f29d974ceb51
sasenjo <sasenjo@clb.t-prisa.com>
2018-07-27 13:08:24 +0200

    Letter Changes Challenge

--- javascriptChallenges.js
+++ javascriptChallenges.js
@@ -28,3 +28,14 @@ function firstReverse(str) {
     return str.split('').reverse().join('');
 }
 console.log(firstReverse('Hello World and Coders'));
+
+/**
+ * Challenge 4
+ * Using the JavaScript language, have the function LetterChanges(str) take the str parameter being passed and modify it using the following algorithm. Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a). Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string.
+ * @param str
+ */
+function letterChanges(str) {
+    let converted = str.replace(/[a-z]/gi, (char) => (char === 'z' || char === 'Z') ? 'a' : String.fromCharCode(char.charCodeAt() + 1));
+    return converted.replace(/a|e|i|o|u/gi, (vowel) => vowel.toUpperCase());
+}
+console.log(letterChanges('fun times!'));
